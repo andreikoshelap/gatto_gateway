31a3159bc1a297c89f77f0807bae0091646d7254
Andrei Koshelap <andreikoshelap@gmail.com>
2025-08-26 19:34:51 +0300

    add xml receiver microservice

---
+++ ws-xml-receiver/src/main/java/com/gatto/ws_xml_receiver/WsXmlReceiverApplication.java
@@ -0,0 +1,13 @@
+package com.gatto.ws_xml_receiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsXmlReceiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsXmlReceiverApplication.class, args);
+	}
+
+}
---
+++ ws-xml-receiver/src/test/java/com/gatto/ws_xml_receiver/WsXmlReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gatto.ws_xml_receiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsXmlReceiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
